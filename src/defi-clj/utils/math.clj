4b88f1f3f80384d8d2d6587273d8ba4c760baf1e
mperrotta <mperrotta@thortech-solutions.com>
2025-06-04 18:15:30 -0400

    got group-byte-array-by-bits working

--- src/defi-clj/utils/math.clj
+++ src/defi-clj/utils/math.clj
@@ -1,38 +1,40 @@
-(ns defi-clj.utils.math)
-
+(ns defi-clj.utils.math
+  (:import [java.util BitSet]))
 
 
 (defn group-byte-array-by-bits
   [bytes group-bit-size]
 
 
-  (loop [[current-byte & rst] bytes
-         current-bits       0
-         current-bit-count  0
-         remaining-bits     0
-         remaining-bit-count 0
-         result             []]
-    (if current-byte
-      (let [needed-bits (- group-bit-size current-bit-count remaining-bit-count)
-            new-bit-count (+ current-bit-count (min 8 needed-bits))
-            current-bits (bit-or (bit-shift-right remaining-bits (- 8 remaining-bit-count)) (bit-shift-left current-bits remaining-bit-count))
-            bits-to-shift (- 8 (min 8 needed-bits))
-            new-bits (-> (bit-shift-right current-byte bits-to-shift)
-                         (bit-or (bit-shift-left current-bits (- 8 bits-to-shift)) ))
-            reamining-bit-count (- 8 bits-to-shift)
-            remaining-bits (bit-shift-left current-byte (- 8 remaining-bit-count))
-            new-result (if (< new-bit-count group-bit-size) result (conj result new-bits))]
+  (let [total-bits (* (count bytes) 8)]
+    (loop [bit-idx            0
+           result             []]
+      (let [bits-left (- total-bits bit-idx 1)]
+        (if (< bit-idx (- total-bits 1))
+          (let [new-bit-count (min group-bit-size bits-left)
+                new-number (reduce
+                            (fn [acc idx]
+                              (let [byte-idx (quot idx 8)
+                                    current-byte (bytes byte-idx)
+                                    byte-bit-idx (- 7 (mod idx 8))
+                                    new-number-bit-idx (mod idx 11)
+                                    current-bit-set (> (bit-and current-byte (bit-shift-left 1 byte-bit-idx)) 0)
+                                    shift-amount (- group-bit-size new-number-bit-idx 1)]
 
-        (println {:current-byte current-byte
-                  :current-bit-count current-bit-count
-                  :remaining-bits remaining-bits
-                  :remaining-bit-count remaining-bit-count
-                  :needed-bits needed-bits
-                  :new-bit-count new-bit-count
-                  :bits-to-shift bits-to-shift
-                  :new-bits new-bits
-                  :new-result new-result})
-        (recur (vec rst) new-bits new-bit-count remaining-bits remaining-bit-count result))
-      (if (> current-bit-count 0)
-        (conj result current-bits)
-        result))))
+                                #_(println idx byte-bit-idx new-number-bit-idx byte-idx current-byte current-bit-set shift-amount acc)
+                                (+ acc (if current-bit-set
+                                         (bit-shift-left 1 shift-amount)
+                                         0))))
+                            0
+                            (range bit-idx (+ bit-idx new-bit-count)))]
+
+            (recur (+ bit-idx new-bit-count) (conj result new-number)))
+          result)))))
+
+
+
+(comment
+
+  (time (group-byte-array-by-bits [0x06 0x36 0x79 0xca 0x1b 0x28 0xb5 0xcf 0xda 0x9c 0x18 0x6b 0x36 0x7e 0x27 0x1e] 11))
+
+  )
